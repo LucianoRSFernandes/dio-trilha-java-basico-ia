65325ad4bf247a2bafe6c983d19979b6e048b27c
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-07-12 11:47:27 -0300

    adicionando exercicio sobre tipos de Variáveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = numero *-1;
+            System.out.println(-numero);
+            System.out.println(numero);
+    }
+}
