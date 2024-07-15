9c92757364f33ca8891252f755faaf43c5e41259
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-07-15 06:02:27 -0300

    adicionando exercício sobre concatenação ao portifólio

---
+++ tipos-variaveis/src/Concatenacao.java
@@ -0,0 +1,15 @@
+public class Concatenacao {
+    public static void main (String[] args) {
+    String nomeCompleto = "LINGUAGEM" + "JAVA";    
+        System.out.println(nomeCompleto);
+    
+    String Concatenacao ="?";
+
+    Concatenacao = 1+1+1+"1";
+        System.out.println(Concatenacao);
+    Concatenacao = 1+1+"1"+1;
+        System.out.println(Concatenacao);
+    Concatenacao = (1+1+1)+"1";
+        System.out.println(Concatenacao);
+      }
+}
