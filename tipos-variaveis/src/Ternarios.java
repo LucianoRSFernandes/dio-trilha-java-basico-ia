bc97ce0d4f6d4fa43ee8358c9bd8c90c11a1725c
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-07-15 19:12:46 -0300

    adicionando exerciício de incrementação com operadores ternarios

---
+++ tipos-variaveis/src/Ternarios.java
@@ -0,0 +1,9 @@
+public class Ternarios {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = a==b ? "verdadeiro" : "false";
+        System.out.println (resultado);
+    }
+}
