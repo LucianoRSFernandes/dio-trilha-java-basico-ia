79cbaf64548bc30e6e20f9ce63d0e4da92cc33c6
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-07-15 11:48:45 -0300

    adicionando exerciício de incrementação boleanos

--- tipos-variaveis/src/Incrementacao.java
+++ tipos-variaveis/src/Incrementacao.java
@@ -1,17 +1,11 @@
 public class Incrementacao {
     public static void main(String[] args) {
-        int numero = 5;
-        //x repeticao
-        numero = numero + 2;
-        System.out.println(numero);
-        numero = numero +1;
-        System.out.println(numero);
-        //numero +1 = numero++
-        numero++;
-        System.out.println(numero);
-        //podemos adicionar o incremento direto no parametro
-        // numero -1 = numero--
-        System.out.println(--numero);
-        System.out.println(numero -2);
+        boolean variavel = true;
+        //o sinal de exclamação inverte o valor da variável
+        System.out.println(!variavel);
+        System.out.println(variavel);
+        // ao adicionarmos o incremento de negação a variável passa a ter o valor contrário definitivo
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
