9b33c9e7e4b9724145a9fb3aafcd0b2a8f348213
lucianorsfernandes <luciano_r_s_fernandes@yahoo.com.br>
2024-08-01 07:15:41 -0300

    Adicionando exercício de Condional Composta com Bloco de instrução dentro de if/else

---
+++ controle-fluxo/src/NovoCaixaEletronico.java
@@ -0,0 +1,14 @@
+//Condicional Composta com bloco de intrução do if
+public class NovoCaixaEletronico {
+    public static void main(String[] args) {
+
+       double saldo = 25.0;
+       double valorSolicitado = 27.0;
+
+       if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;            
+        System.out.println ("Novo saldo" + saldo);
+       }else
+        System.out.println("Saldo insuficiente");
+    }
+}
